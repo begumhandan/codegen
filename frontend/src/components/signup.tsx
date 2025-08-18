6205fb1c2bebbe9ba531a03c5cf9ca0b56c44223
Begum Handan Demir <begumhandandemir@gmail.com>
2025-08-18 17:19:22 +0300

    kullanıcı kayıt olma sayfası yazıldı

---
+++ frontend/src/components/signup.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { useHandleSubmit } from "@/components/hooks/useSignUp/useHandleSubmit";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+
+export function Signup({ className, ...props }: React.ComponentProps<"div">) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { handleSubmit } = useHandleSubmit();
+
+  return (
+    <div className={cn("p-[10px] md:p-[2rem] max-w-sm flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Sign Up</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={(e) => handleSubmit(e, setIsLoading)}>
+            <div className="grid gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="elizamccardlejohson@robust.com"
+                  required
+                  disabled={isLoading}
+                />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                  required
+                  disabled={isLoading}
+                />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="role">Rol</Label>
+                <Input id="role" name="role" type="text" placeholder="Rolünüzü girin" required disabled={isLoading} />
+              </div>
+
+              <Button type="submit" className="w-full bg-black text-white hover:bg-gray-800" disabled={isLoading}>
+                {isLoading ? "Kayıt yapılıyor..." : "Kayıt Ol"}
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="">Terms of Service</a> and <a href="">Privacy Policy</a>.
+      </div>
+    </div>
+  );
+}
