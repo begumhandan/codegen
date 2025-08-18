8be26d346646fa8bc6ae3d3c5860a9ea7aeb1088
Begum Handan Demir <begumhandandemir@gmail.com>
2025-08-18 17:33:47 +0300

    kullanıcı kayıt olma route eklendi

---
+++ frontend/src/routes/signup.tsx
@@ -0,0 +1,16 @@
+import { Signup } from "@/components/signup";
+import { createFileRoute } from "@tanstack/react-router";
+export const Route = createFileRoute("/signup")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="" className="flex items-center gap-2 self-center font-medium"></a>
+        <Signup />
+      </div>
+    </div>
+  );
+}
