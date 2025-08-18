b593bd0cc343d07c384944cd0e60542c0bbb1f2a
Begum Handan Demir <begumhandandemir@gmail.com>
2025-08-18 16:59:10 +0300

    Navigasyon için sayfalar tanımlandı

---
+++ frontend/src/components/hooks/useNavigatePages.tsx
@@ -0,0 +1,29 @@
+import { useRouterState } from "@tanstack/react-router";
+
+export const useNavigatePages = () => {
+  const { location } = useRouterState();
+
+  const pages = [
+    "/",
+    "/anasayfa",
+    "/Cmm",
+    "/elektriksel_test",
+    "/signup",
+    "/sayfa3",
+    "/sayfa4",
+    "/sayfa5",
+    "/sayfa6",
+    "/sayfa7",
+    "/sayfa8",
+    "/sayfa9",
+  ];
+
+  const currentPath = location.pathname;
+  const currentIndex = pages.indexOf(currentPath);
+
+  return {
+    pages,
+    currentPath,
+    currentIndex,
+  };
+};
