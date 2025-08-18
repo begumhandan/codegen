cd84144fd0a117bcd18c4ee3e11578cb920aff68
Begum Handan Demir <begumhandandemir@gmail.com>
2025-08-18 17:18:53 +0300

    kullanıcı giriş yapma sayfası yazıldı

---
+++ frontend/src/components/login.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { useHandleSubmit } from "@/components/hooks/useLogin/useHandleSubmit";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+
+export function Login({ className, ...props }: React.ComponentProps<"div">) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { handleSubmit } = useHandleSubmit();
+
+  return (
+    <div className={cn("p-[10px] md:p-[2rem] max-w-sm flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Kullanıcı Giriş</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={(e) => handleSubmit(e, setIsLoading)}>
+            <div className="grid gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" name="email" type="email" placeholder="m@example.com" required disabled={isLoading} />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Şifre</Label>
+                  <a href="/forgot-password" className="ml-auto text-sm underline-offset-4 hover:underline">
+                    Forget password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                  required
+                  disabled={isLoading}
+                />
+              </div>
+              <Button type="submit" className="w-full bg-black text-white hover:bg-gray-800" disabled={isLoading}>
+                {isLoading ? "Giriş yapılıyor..." : "Log In"}
+              </Button>
+            </div>
+            <div className="text-center text-sm mt-4">
+              Don't have an account?
+              <br />
+              <a href="/signup" className="underline underline-offset-4 hover:text-primary">
+                Sign Up
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="">Terms of Service</a> and <a href="">Privacy Policy</a>.
+      </div>
+    </div>
+  );
+}
