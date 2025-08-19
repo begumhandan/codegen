e23f3e259102e8d55ab4e7dc7a4559514e0f1551
Begum Handan Demir <begumhandandemir@gmail.com>
2025-08-19 10:22:50 +0300

    Elektriksel Test sayfası yazıldı

---
+++ frontend/src/components/elektriksel_test.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { CreateETCard } from "./ETs/CreateETCard";
+import { DeleteETCard } from "./ETs/DeleteETcard";
+import { Label } from "./ui/label";
+
+const InputIconButtonDemo = () => {
+  const [currentUser, setCurrentUser] = useState<any>(null);
+
+  useEffect(() => {
+    const userData = localStorage.getItem("user");
+    if (userData) {
+      const parsedUser = JSON.parse(userData);
+      setCurrentUser(parsedUser);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(currentUser);
+  }, [currentUser]);
+
+  return (
+    <>
+      <div className="flex justify-between mb-2">
+        <Label className="text-left mx-5 my-5">ET</Label>
+        <Label className="text-right capitalize mx-5 my-5">{currentUser && `Role: ${currentUser.role}`}</Label>
+      </div>
+      <div className="bg-muted min-h-svh grid grid-cols-1 md:grid-cols-2 gap-6 p-6 md:p-10 place-items-center">
+        <CreateETCard />
+        <DeleteETCard />
+      </div>
+    </>
+  );
+};
+export function ElektrikselTest() {
+  return <InputIconButtonDemo />;
+}
