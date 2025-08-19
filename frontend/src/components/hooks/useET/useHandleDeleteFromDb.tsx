4a3156b1c3a15a69ff8a91ee014cab2e0092ab3e
Begum Handan Demir <begumhandandemir@gmail.com>
2025-08-19 10:20:15 +0300

    Elektriksel Test kodunu databaseden silme kısmı yazıldı

---
+++ frontend/src/components/hooks/useET/useHandleDeleteFromDb.tsx
@@ -0,0 +1,28 @@
+import { deleteET } from "@/api/electrical_testing";
+
+export const useHandleDeleteFromDB = () => {
+  //ET kodunu databaseden kod silme
+  const handleDeleteFromDB = async (currentCode: string, currentUser: any, setCurrentCode: (code: string) => void) => {
+    currentCode = currentCode.replace("ET-", "");
+    if (!currentCode) {
+      alert("Silinecek kod yok!");
+      return;
+    }
+
+    if (!currentUser) {
+      alert("Giriş yapmanız gerekiyor!");
+      return;
+    }
+
+    try {
+      await deleteET(currentCode, currentUser.id);
+      setCurrentCode("");
+      alert("Kod database'den silindi!");
+    } catch (error) {
+      console.error("Delete error:", error);
+      alert("Bu kodu silme yetkiniz yok veya kod bulunamadı!");
+    }
+  };
+
+  return { handleDeleteFromDB };
+};
