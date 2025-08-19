353c6ffefa1f63122a3e75158b84de8cf0efa307
Begum Handan Demir <begumhandandemir@gmail.com>
2025-08-19 09:47:23 +0300

    type eklemeler yapıldı

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -4,3 +4,36 @@ export interface User {
   role: string;
   createdAt?: string;
 }
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  success: boolean;
+  user: User;
+}
+
+export interface RegisterRequest {
+  email: string;
+  password: string;
+  role: string;
+}
+
+export interface GenerateCMMRequest {
+  user_id: number;
+}
+export interface GenerateETRequest {
+  user_id: number;
+}
+export interface CMMResponse {
+  success: boolean;
+  code: string;
+  process: any;
+}
+export interface ETResponse {
+  success: boolean;
+  code: string;
+  process: any;
+}
