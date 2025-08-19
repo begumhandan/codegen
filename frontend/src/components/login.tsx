5705c2589b05115ff390ea1cd96901d45f086331
Begum Handan Demir <begumhandandemir@gmail.com>
2025-08-19 09:41:06 +0300

    yazım düzenlendi

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { useHandleSubmit } from "@/components/hooks/useLogin/useHandleSubmit";
 import { Button } from "@/components/ui/button";
+import { useHandleSubmit } from "@/components/hooks/useLogIn/useHandleSubmit";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
