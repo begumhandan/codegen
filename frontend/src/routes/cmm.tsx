410d977e10c9ae74d494eba61d9570967afd9680
Begum Handan Demir <begumhandandemir@gmail.com>
2025-08-21 08:42:40 +0300

    cmm card route eklendi

--- frontend/src/routes/cmm.tsx
+++ frontend/src/routes/cmm.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Cmm } from "@/components/cmm";
+import { CmmCodesManagement } from "@/components/Cmm/deleteCmmCodeCard";
+import { CodegenCard } from "@/components/custom/codegen-card";
 
 export const Route = createFileRoute("/cmm")({
   component: RouteComponent,
@@ -7,8 +8,11 @@ export const Route = createFileRoute("/cmm")({
 
 function RouteComponent() {
   return (
-    <div>
-      <Cmm />
+    <div className="h-dvh w-full flex items-center justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-6 md:p-10 place-items-center">
+        <CodegenCard prefix="CMM" />
+        <CmmCodesManagement />
+      </div>
     </div>
   );
 }
