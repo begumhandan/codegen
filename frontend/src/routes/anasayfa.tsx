653872d7ac0eb51bf3884fc9ce92c91bff83bc2f
Begum Handan Demir <begumhandandemir@gmail.com>
2025-08-21 08:41:34 +0300

    anasayfa header eklendi

--- frontend/src/routes/anasayfa.tsx
+++ frontend/src/routes/anasayfa.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Anasayfa } from "@/components/anasayfa";
+import { Header } from "@/components/layout/header";
 
 export const Route = createFileRoute("/anasayfa")({
   component: RouteComponent,
@@ -8,6 +9,7 @@ export const Route = createFileRoute("/anasayfa")({
 function RouteComponent() {
   return (
     <div>
+      <Header />
       <Anasayfa />
     </div>
   );
