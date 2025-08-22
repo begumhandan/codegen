1ea3a03a2f4009b55091947a3daa283ee63770f9
Begum Handan Demir <begumhandandemir@gmail.com>
2025-08-22 10:47:12 +0300

    cmm schema silindi

--- backend/src/db/CmmSchema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const CmmSchema = z.object({
-  code: z.string(),
-  created_by: z.number(),
-  created_at: z.string().transform((val: string) => new Date(val)),
-});
